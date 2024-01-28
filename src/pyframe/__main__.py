bf223d216c946ce1bcc7c459044aea3f33c10aee
Bernd Kalbfuss <langweiler@rueblitorte.de>
2024-01-28 16:05:16 +0100

    Removed setting of environment variables.

--- src/pyframe/__main__.py
+++ src/pyframe/__main__.py
@@ -5,16 +5,6 @@ License: GNU General Public License v3 (GPLv3)
 """
 
 import argparse
-import os
-
-
-# Use gl4es wrapper if available
-if 'LD_LIBRARY_PATH' in os.environ:
-    os.environ['LD_LIBRARY_PATH'] = f"{ os.environ['LD_LIBRARY_PATH'] }:/usr/lib/gl4es/"
-else:
-    os.environ['LD_LIBRARY_PATH'] = "/usr/lib/gl4es/"
-# Use gstreamer for playing video
-os.environ['KIVY_VIDEO'] = "gstplayer"
 
 
 # Parse arguments
